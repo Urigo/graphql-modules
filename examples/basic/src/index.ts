8cbe615b6eb2b260ee41334b4b8430f700940d0c
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-07-12 10:10:54 +0200

    make sure Basic example runs

--- examples/basic/src/index.ts
+++ examples/basic/src/index.ts
@@ -5,20 +5,28 @@ import { socialNetworkModule } from './modules/social-network';
 import { authModule } from './modules/auth';
 import { GraphQLApp } from '@graphql-modules/core';
 
-const gqlApp = new GraphQLApp({
-  modules: [
-    userModule,
-    socialNetworkModule,
-    authModule,
-  ],
-});
+async function run() {
+  const gqlApp = new GraphQLApp({
+    modules: [
+      userModule,
+      socialNetworkModule,
+      authModule,
+    ],
+  });
 
-const app = express();
+  await gqlApp.init();
 
-app.use('/graphql', graphqlHTTP(async req => ({
-  schema: gqlApp.schema,
-  graphiql: true,
-  context: await gqlApp.buildContext(req),
-})));
+  const app = express();
 
-app.listen(4000);
+  app.use('/graphql', graphqlHTTP(async req => ({
+    schema: gqlApp.schema,
+    graphiql: true,
+    context: await gqlApp.buildContext(req),
+  })));
+
+  app.listen(4000, () => {
+    console.log('Visit http://localhost:4000/graphql');
+  });
+}
+
+run();
