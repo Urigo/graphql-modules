93fa3fc499a4f5288312ec291cd2b2a8a1cff759
dotansimha <dotansimha@gmail.com>
2018-07-17 09:29:57 +0300

    export basic context interface

--- packages/core/src/di/types.ts
+++ packages/core/src/di/types.ts
@@ -1,20 +1,30 @@
 import { interfaces } from 'inversify';
 
-export { injectable, inject, optional, LazyServiceIdentifer as Lazy } from 'inversify';
+export { injectable, inject, optional } from 'inversify';
+
 export interface Type<T> extends Function {
-  new (...args: any[]): T;
+  new(...args: any[]): T;
 }
+
 export interface ValueProvider {
   provide: any;
   useValue: any;
 }
+
 export interface ClassProvider {
   provide: any;
   useClass: Type<any>;
 }
-export interface TypeProvider extends Type<any> {}
+
+export interface TypeProvider extends Type<any> {
+}
+
 export type Provider = TypeProvider | ValueProvider | ClassProvider | any[];
 
 export interface Injector {
-    get<T>(serviceIdentifier: interfaces.ServiceIdentifier<T>): T;
+  get<T>(serviceIdentifier: interfaces.ServiceIdentifier<T>): T;
+}
+
+export interface AppContext {
+  injector: Injector;
 }
