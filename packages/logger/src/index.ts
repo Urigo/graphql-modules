7efd94102c7efcb749483bdd1e36b7633df43793
dotansimha <dotansimha@gmail.com>
2018-07-23 16:38:47 +0300

    fix logger

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,12 +1,23 @@
 import * as winston from 'winston';
-import { createLogger, Logger, format } from 'winston';
+import { createLogger, format, Logger } from 'winston';
+import { inspect } from 'util';
 
 export const logger: Logger = createLogger({
   level: 'debug',
-  transports: [ new winston.transports.Console()],
+  transports: [new winston.transports.Console()],
   format: format.combine(
-    format.timestamp(),
+    format.metadata(),
     format.colorize(),
+    format.timestamp(),
+    format.printf(nfo => {
+      let data = '';
+
+      if (nfo.metadata && ((typeof nfo.metadata === 'object' && Object.keys(nfo.metadata).length > 0) || Array.isArray(nfo))) {
+        data = '\n' + inspect(nfo.metadata, { depth: null, colors: true });
+      }
+
+      return `[${nfo.timestamp}][${nfo.level}]: ${nfo.message}${data}`;
+    }),
   ),
 });
 
