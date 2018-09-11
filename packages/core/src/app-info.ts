65f371083619cd74813219df6e8aa42c738334c0
dotansimha <dotansimha@gmail.com>
2018-09-11 09:25:18 +0200

    docs cleanup

--- packages/core/src/app-info.ts
+++ packages/core/src/app-info.ts
@@ -13,14 +13,22 @@ export class AppInfo {
     this.app = app;
   }
 
+  /** Returns the current network request. The request object comes from your network
+   * library (such as `connect` or `express`).
+   * @return network request object
+   */
   public getRequest(): any {
     return this.request;
   }
 
+  /** Returns the current GraphQL execution `context`.
+   */
   public getContext(): IGraphQLContext {
     return this.context;
   }
 
+  /** Returns the current `GraphQLApp` your are running it.
+   */
   public getApp(): GraphQLApp {
     return this.app;
   }
