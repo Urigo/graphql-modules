ee0808b6a628fc6df811c7cf3420829a97b5068e
DAB0mB <emanor6@gmail.com>
2018-10-17 21:03:21 +0800

    Update header and footer design

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -21,70 +21,32 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="nav-footer" id="footer">
-        <section className="sitemap">
-          <a href={this.props.config.baseUrl} className="nav-home">
-            {this.props.config.footerIcon && (
-              <img
-                src={this.props.config.baseUrl + this.props.config.footerIcon}
-                alt={this.props.config.title}
-                width="66"
-                height="58"
-              />
-            )}
-          </a>
-          <div>
-            <h5>Docs</h5>
-            <a href={this.docUrl('introduction/getting-started', this.props.language)}>
-              Getting Started
-            </a>
-            <a href={this.docUrl('introduction/modules', this.props.language)}>
-              Documentation
-            </a>
-            <a href={this.docUrl('api/core/api-readme', this.props.language)}>
-              API Reference
-            </a>
-          </div>
-          <div>
-            <h5>Community</h5>
-            <a
-              href="http://stackoverflow.com/questions/tagged/graphql-modules"
-              target="_blank"
-              rel="noreferrer noopener">
-              Stack Overflow
-            </a>
-          </div>
-          <div>
-            <h5>More</h5>
-            <a href="https://github.com/Urigo/graphql-modules">GitHub</a>
-            <a href="https://github.com/Urigo/graphql-modules/issues">Issues</a>
-            <a href="https://github.com/Urigo/graphql-modules/releases">Releases</a>
-            <a
-              className="github-button"
-              href={this.props.config.repoUrl}
-              data-icon="octicon-star"
-              data-count-href="/Urigo/graphql-modules/stargazers"
-              data-show-count="true"
-              data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star this project on GitHub">
-              Star
-            </a>
-          </div>
-        </section>
-
-        <section className="copyright">Website Built With Docusaurus</section>
-        <a
-          href="https://code.facebook.com/projects/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="fbOpenSource">
-          <img
-            src={`${this.props.config.baseUrl}img/oss_logo.png`}
-            alt="Facebook Open Source"
-            width="120"
-            height="30"
-          />
+        <a href={this.props.config.baseUrl} className="nav-home">
+          {this.props.config.footerIcon && (
+            <img
+              src={this.props.config.baseUrl + this.props.config.footerIcon}
+              alt={this.props.config.title}
+              width="145"
+              height="23"
+            />
+          )}
         </a>
-        <section className="copyright">{this.props.config.copyright}</section>
+        <div className="navMenu">
+          <a href={this.docUrl('introduction/getting-started', this.props.language)}>
+            GETTING STARTED
+          </a>
+          <span>⦁</span>
+          <a href={this.docUrl('introduction/modules', this.props.language)}>
+            DOCUMENTATION
+          </a>
+          <span>⦁</span>
+          <a href={this.docUrl('api/core/api-readme', this.props.language)}>
+            API REFERENCE
+          </a>
+        </div>
+        <div className="navCopyright">
+          All rights reserved © 2018 The Guild
+        </div>
       </footer>
     );
   }
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -31,11 +31,11 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { doc: 'introduction/getting-started', label: 'Docs' },
-    { doc: 'api/core/api-readme', label: 'API Reference' },
+    { doc: 'api/core/api-readme', label: 'API REFERENCE' },
+    { doc: 'introduction/getting-started', label: 'DOCUMENTATION' },
     { search: true },
-    { page: 'help', label: 'Help' },
-    { href: 'https://github.com/Urigo/graphql-modules', label: 'GitHub' },
+    { page: 'help', label: 'SUPPORT' },
+    { href: 'https://github.com/Urigo/graphql-modules', label: 'GITHUB' },
   ],
 
   // If you have users set above, you add it here:
@@ -48,7 +48,7 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#181E25',
+    primaryColor: '#13114a',
     secondaryColor: '#181E25',
   },
 
