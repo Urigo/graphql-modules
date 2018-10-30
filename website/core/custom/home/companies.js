cb6eab0dc0901d3cdc7dba5b54e222257b6de5c7
DAB0mB <emanor6@gmail.com>
2018-10-30 19:08:21 +0800

    Use watermarked companies logos

--- website/core/custom/home/companies.js
+++ website/core/custom/home/companies.js
@@ -6,16 +6,16 @@ const List = require('../list')
 
 const airfranceLogo = `${siteConfig.baseUrl}img/home/companies/airfrance-logo.svg`
 const klmLogo = `${siteConfig.baseUrl}img/home/companies/klm-logo.svg`
-const msjLogo = `${siteConfig.baseUrl}img/home/companies/msj-logo.png`
+const msjLogo = `${siteConfig.baseUrl}img/home/companies/msj-logo.svg`
 const schneiderLogo = `${siteConfig.baseUrl}img/home/companies/schneider-logo.svg`
 
 module.exports = (props) => (
   <div {...props} className={`Companies ${props.className || ''}`}>
     <List className="_companies-list">
-      <List.GreedyItem className="_company"><img style={{ height: '70px' }} src={airfranceLogo} alt="airfrance" /></List.GreedyItem>
-      <List.GreedyItem className="_company"><img style={{ height: '80px' }} src={klmLogo} alt="klm" /></List.GreedyItem>
-      <List.GreedyItem className="_company"><img style={{ height: '90px' }} src={msjLogo} alt="msj" /></List.GreedyItem>
-      <List.GreedyItem className="_company"><img style={{ height: '70px' }} src={schneiderLogo} alt="schneider" /></List.GreedyItem>
+      <List.GreedyItem className="_company"><img src={airfranceLogo} alt="airfrance" /></List.GreedyItem>
+      <List.GreedyItem className="_company"><img src={klmLogo} alt="klm" /></List.GreedyItem>
+      <List.GreedyItem className="_company"><img src={msjLogo} alt="msj" /></List.GreedyItem>
+      <List.GreedyItem className="_company"><img src={schneiderLogo} alt="schneider" /></List.GreedyItem>
     </List>
   </div>
 )
