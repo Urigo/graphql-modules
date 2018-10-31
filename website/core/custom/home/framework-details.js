f36c1f9eddeb04b72a954e230efcf60bf25ebe48
DAB0mB <emanor6@gmail.com>
2018-10-31 22:39:10 +0800

    Change to Apollo Inside

--- website/core/custom/home/framework-details.js
+++ website/core/custom/home/framework-details.js
@@ -8,10 +8,10 @@ const apolloLogo = `${siteConfig.baseUrl}img/home/companies/apollo-logo.png`
 
 module.exports = (props) => (
   <div {...props} className={`FrameworkDetails ${props.className || ''}`}>
+    <img className="_bg-workers" src={workers} alt="" />
     <div className="_details">
-      <div className="_title"><span>Powered by</span><img src={apolloLogo} className="_apollo-logo" /></div>
+      <div className="_title"><img src={apolloLogo} className="_apollo-logo" /><span>Inside</span></div>
       <div className="_subtitle">GraphQL Modules is a set of extra tools, structures and guidelines around the amazing Apollo Server 2.0. You’ll see how effective those tools are once you’ll start growing and scaling your GraphQL server.</div>
     </div>
-    <img className="_bg-workers" src={workers} alt="" />
   </div>
 )
