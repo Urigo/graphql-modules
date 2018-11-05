ff9561353df02b70452dbe3c6415def672a007dc
DAB0mB <emanor6@gmail.com>
2018-11-05 18:15:53 +0800

    Update contact text

--- website/core/custom/home/contact-us.js
+++ website/core/custom/home/contact-us.js
@@ -1,12 +1,13 @@
 const React = require('react')
 
+const siteConfig = require('../../../siteConfig')
 const ContactForm = require('./contact-form')
 
 const ContactUs = () => (
   <div className="ContactUs">
     <h2 className="_kicker">Need Help?</h2>
     <h1 className="_title">We've Got You Covered!</h1>
-    <div className="_subtitle">Get our team's help with Apollo, GraphQL and GraphQL Modules. Whether you’re just getting started or rolling out GraphQL across your whole organization, we can help with architectural design, implementation and education.</div>
+    <div className="_subtitle">Check out our <a href={`${siteConfig.baseUrl}docs/introduction/getting-started`}>docs</a>, open an issue on our <a href={siteConfig.githubUrl}>GitHub repo</a> or simply contact us directly! We would love to help you with Apollo, GraphQL and GraphQL Modules and anything in between! We can help you get started or implement GraphQL across your whole organization.</div>
     <ContactForm />
   </div>
 )
