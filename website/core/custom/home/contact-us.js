f7a9aa70d0d972af4b93d62223f61de297e8a014
Uri Goldshtein <uri.goldshtein@gmail.com>
2018-11-05 12:44:18 +0000

    Update contact-us.js

--- website/core/custom/home/contact-us.js
+++ website/core/custom/home/contact-us.js
@@ -7,7 +7,7 @@ const ContactUs = () => (
   <div className="ContactUs">
     <h2 className="_kicker">Need Help?</h2>
     <h1 className="_title">We've Got You Covered!</h1>
-    <div className="_subtitle">Check out our <a href={`${siteConfig.baseUrl}docs/introduction/getting-started`}>docs</a>, open an issue on our <a href={siteConfig.githubUrl}>GitHub repo</a> or simply contact us directly! We would love to help you with Apollo, GraphQL and GraphQL Modules and anything in between! We can help you get started or implement GraphQL across your whole organization.</div>
+    <div className="_subtitle">Check out our <a href={`${siteConfig.baseUrl}docs/introduction/getting-started`}>docs</a>, open an issue on our <a href={siteConfig.githubUrl}>GitHub repo</a> or simply contact us directly! We would love to help you with Apollo, GraphQL and GraphQL Modules and anything in between! We can help you get started or scale GraphQL across your whole organization.</div>
     <ContactForm />
   </div>
 )
