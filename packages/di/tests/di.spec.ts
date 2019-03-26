519118871b9e0c7b36b5e688eed0923ba21dfe9a
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-03-26 17:48:30 +0100

    Make sure DI works

--- packages/di/tests/di.spec.ts
+++ packages/di/tests/di.spec.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import { Injectable, Injector } from '../src';
+import { Injectable, Injector, ProviderScope } from '../src';
 
 describe('Dependency Injection', () => {
   it('clear instances if provider is overwritten', async () => {
@@ -22,5 +22,49 @@ describe('Dependency Injection', () => {
       },
     });
     expect(injector.get(FooProvider).foo()).toBe('BAR');
+  });
+
+  it('should lazily create providers', async () => {
+    const initSpy = jest.fn();
+
+    class Base {
+      onRequest() {}
+    }
+
+    @Injectable({
+      scope: ProviderScope.Session,
+    })
+    class FooProvider extends Base {
+      constructor() {
+        super();
+        initSpy('foo');
+      }
+
+      foo() {
+        return 'FOO';
+      }
+    }
+
+    @Injectable({
+      scope: ProviderScope.Session,
+    })
+    class BarProvider extends Base {
+      constructor() {
+        super();
+        initSpy('bar');
+      }
+
+      bar() {
+        return 'BAR';
+      }
+    }
+
+    const injector = new Injector();
+
+    injector.provide(FooProvider);
+    injector.provide(BarProvider);
+
+    expect(injector.get(FooProvider).foo()).toBe('FOO');
+    expect(initSpy).toHaveBeenCalledTimes(1);
   });
 });
