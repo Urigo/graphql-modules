4d7d9049b0eb42cd62c8bd41b9c456b77166649b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-04-01 11:17:39 -0400

    Add Memory Leak tests

--- packages/di/tests/di.spec.ts
+++ packages/di/tests/di.spec.ts
@@ -1,26 +1,32 @@
 import 'reflect-metadata';
 import { Injectable, Injector } from '../src';
+import { iterate } from 'leakage';
 
 describe('Dependency Injection', () => {
-  it('clear instances if provider is overwritten', async () => {
-    @Injectable()
-    class FooProvider {
-      foo() {
-        return 'FOO';
-      }
-    }
-    const injector = new Injector();
-    injector.provide(FooProvider);
-    expect(injector.get(FooProvider).foo()).toBe('FOO');
-    injector.provide({
-      provide: FooProvider,
-      overwrite: true,
-      useValue: {
+  it('clear instances if provider is overwritten', () => {
+    iterate(() => {
+      @Injectable()
+      class FooProvider {
         foo() {
-          return 'BAR';
+          return 'FOO';
+        }
+      }
+      const injector = new Injector({
+        initialProviders: [
+          FooProvider,
+        ],
+      });
+      expect(injector.get(FooProvider).foo()).toBe('FOO');
+      injector.provide({
+        provide: FooProvider,
+        overwrite: true,
+        useValue: {
+          foo() {
+            return 'BAR';
+          },
         },
-      },
+      });
+      expect(injector.get(FooProvider).foo()).toBe('BAR');
     });
-    expect(injector.get(FooProvider).foo()).toBe('BAR');
   });
 });
