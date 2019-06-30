bb30502e5756950db76de070e6116b82121161e1
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-06-30 15:47:28 -0400

    Add webpack example

---
+++ examples/basic-webpack/src/app/app.module.ts
@@ -0,0 +1,8 @@
+import { GraphQLModule } from '@graphql-modules/core';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { SocialNetworkModule } from './social-network/social-network.module';
+
+export const AppModule = new GraphQLModule({
+  imports: [AuthModule, UserModule, SocialNetworkModule]
+});
