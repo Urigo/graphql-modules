9597b0570be48b54ca7f393b193ead21c8df17c4
Mohamed Gamal <Mohamed.abugalala@gmail.com>
2020-03-12 00:07:28 +0200

    Fix Example(basic-with-dependency-injection) (#952)
    * Install missing dependency -- graphql-tag
    * Fix importing module in index.ts and enhance it a little
    
    Co-authored-by: Mohamed Gamal <mohamed@vrolio.com>

--- examples/basic-with-dependency-injection/src/modules/app/app.module.ts
+++ examples/basic-with-dependency-injection/src/modules/app/app.module.ts
@@ -1,6 +1,6 @@
 import { GraphQLModule } from '@graphql-modules/core';
-import { UserModule } from '../user';
-import { BlogModule } from '../blog';
+import { UserModule } from '@modules/user';
+import { BlogModule } from '@modules/blog';
 
 export const AppModule = new GraphQLModule({
   imports: [
