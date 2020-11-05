cf4d10444e0abda9cb80425c01c39c81b8aab228
liaoliaojun <1030219089a@gmail.com>
2020-11-06 02:43:08 +0800

    fix(examples): use express-graphql 'graphqlHTTP' (#1404)

--- examples/basic-with-dependency-injection-js/src/index.js
+++ examples/basic-with-dependency-injection-js/src/index.js
@@ -2,7 +2,7 @@ require('reflect-metadata');
 const { BlogModule } = require('./modules/blog');
 const { UserModule } = require('./modules/user');
 const express = require('express');
-const graphQLHTTP = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 
 const app = createApplication({
   modules: [BlogModule, UserModule],
@@ -12,11 +12,14 @@ const server = express();
 
 const execute = app.createExecution();
 
-server.use('/graphql', graphQLHTTP({
-  schema: AppModule.schema,
-  customExecuteFn: execute,
-  graphiql: true,
-}));
+server.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: AppModule.schema,
+    customExecuteFn: execute,
+    graphiql: true,
+  })
+);
 
 server.listen(4000, () => {
   console.log('Live http://localhost:4000/graphql');
