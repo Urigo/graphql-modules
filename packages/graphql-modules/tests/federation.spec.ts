8191027a9aed27df8dedc21d1b8623d9725ee8ad
Kamil Kisiela <kamil.kisiela@gmail.com>
2021-03-02 11:14:22 +0000

    Test __resolveReference

---
+++ packages/graphql-modules/tests/federation.spec.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import { buildFederatedSchema } from '@apollo/federation';
+import { mergeResolvers } from '@graphql-tools/merge';
+import { createApplication, createModule, gql } from '../src';
+
+test('allow __resolveReference', async () => {
+  const mod = createModule({
+    id: 'test',
+    typeDefs: gql`
+      type Query {
+        me: User!
+      }
+
+      type User {
+        id: ID!
+        username: String
+      }
+    `,
+    resolvers: {
+      User: {
+        __resolveReference() {},
+      },
+    },
+  });
+
+  expect(() =>
+    createApplication({
+      modules: [mod],
+      schemaBuilder(input) {
+        return buildFederatedSchema({
+          typeDefs: input.typeDefs,
+          resolvers: mergeResolvers(input.resolvers),
+        });
+      },
+    })
+  ).not.toThrow();
+});
