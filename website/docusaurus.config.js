021ae2bc5d244b8d0ad55b4131c2727214ab163c
Kamil Kisiela <kamil.kisiela@gmail.com>
2021-04-07 07:46:28 +0000

    Fix mult-website search

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -178,11 +178,6 @@ module.exports = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-        sitemap: {
-          cacheTime: 600 * 1001, // 600 sec - cache purge period
-          changefreq: 'weekly',
-          priority: 0.5,
-        },
       },
     ],
   ],
