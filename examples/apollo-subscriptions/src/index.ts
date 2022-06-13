1e698c3a7d89810945a78977fbe82350d48d01a3
Arda TANRIKULU <ardatanrikulu@gmail.com>
2022-06-13 01:46:20 -0400

    Replace createSchemaForApollo with createApolloExecutor in docs (#2204)

--- examples/apollo-subscriptions/src/index.ts
+++ examples/apollo-subscriptions/src/index.ts
@@ -2,26 +2,54 @@ import 'reflect-metadata';
 import express from 'express';
 import { createServer } from 'http';
 import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import { graphqlApplication } from './app';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 
-const { createSchemaForApollo } = graphqlApplication;
-const schema = createSchemaForApollo();
+const { schema, createExecution, createSubscription, createApolloExecutor } =
+  graphqlApplication;
+
+const executor = createApolloExecutor();
+
+const app = express();
+const httpServer = createServer(app);
+// Creating the WebSocket subscription server
 
 const server = new ApolloServer({
   schema,
+  executor,
+  plugins: [
+    // Proper shutdown for the HTTP server.
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+    // Proper shutdown for the WebSocket server.
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
+    },
+  ],
 });
 
-const app = express();
 server.applyMiddleware({ app });
 
-const httpServer = createServer(app);
-server.installSubscriptionHandlers(httpServer);
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: server.graphqlPath,
+});
+
+const execute = createExecution();
+const subscribe = createSubscription();
+
+// Passing in an instance of a GraphQLSchema and
+// telling the WebSocketServer to start listening
+const serverCleanup = useServer({ schema, execute, subscribe }, wsServer);
 
 httpServer.listen({ port: 4000 }, () => {
   // tslint:disable-next-line: no-console
   console.info(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
-  // tslint:disable-next-line: no-console
-  console.info(
-    `🚀 Subsciription ready at ws://localhost:4000${server.subscriptionsPath}`
-  );
 });
--- examples/subscriptions/src/index.ts
+++ examples/subscriptions/src/index.ts
@@ -1,13 +1,10 @@
 import 'reflect-metadata';
 import { createServer } from 'http';
-import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 import { app } from './app';
 
 const WS_PORT = 5000;
-
-const schema = app.schema;
-const execute = app.createExecution();
-const subscribe = app.createSubscription();
 
 // Create WebSocket listener server
 const websocketServer = createServer((_request, response) => {
@@ -19,15 +16,20 @@ const websocketServer = createServer((_request, response) => {
 websocketServer.listen(WS_PORT, () => {
   console.log(`Live http://localhost:${WS_PORT}/graphql`);
 });
+const wsServer = new WebSocketServer({
+  port: 4000,
+  path: '/graphql',
+});
 
-SubscriptionServer.create(
+const schema = app.schema;
+const execute = app.createExecution();
+const subscribe = app.createSubscription();
+
+useServer(
   {
     schema,
     execute,
     subscribe,
   },
-  {
-    server: websocketServer,
-    path: '/graphql',
-  }
+  wsServer
 );
