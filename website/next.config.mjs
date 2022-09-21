608fe6ff725f8476a9541f20928250778d0c0e87
TuvalSimha <tuval.simha@gmail.com>
2022-09-21 16:54:34 +0300

    Unified Websites: update
    Fix
    
    fix
    
    fix
    
    fix fix
    
    fixxxxx
    
    fixxxx
    
    fix?
    
    fix
    
    content fixes

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,3 +1,14 @@
 import { withGuildDocs } from '@theguild/components/next.config';
 
-export default withGuildDocs();
+export default withGuildDocs({
+  images: {
+    unoptimized: true, // doesn't work with `next export`
+    allowFutureImage: true,
+  },
+  redirects: () =>
+    Object.entries({}).map(([from, to]) => ({
+      source: from,
+      destination: to,
+      permanent: true,
+    })),
+});
