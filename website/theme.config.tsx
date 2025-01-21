88c309fbc23d8fe4f7ad859442bfe5586e696d80
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2025-01-21 22:27:19 +0700

    more

--- website/theme.config.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint sort-keys: error */
-import { defineConfig, Giscus, PRODUCTS, useTheme } from '@theguild/components';
-import { useRouter } from 'next/router';
-
-export default defineConfig({
-  description: 'GraphQL server tooling',
-  logo: PRODUCTS.MODULES.logo({ className: 'w-9' }),
-  websiteName: 'GraphQL-Modules',
-});
